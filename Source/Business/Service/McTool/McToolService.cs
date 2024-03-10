cac97ff3905b1c94780a87ad5a34f481e9094226
DarkCompet <darkcompet@gmail.com>
2024-03-10 21:50:28 +0700

    Omit dbcontext from mctoolservice

--- Source/Business/Service/McTool/McToolService.cs
+++ Source/Business/Service/McTool/McToolService.cs
@@ -1,30 +1,25 @@
 namespace App;
 
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
-public class McToolService : BaseService {
+public class McToolService {
 	private readonly ILogger<McToolService> logger;
 
 	public McToolService(
-		AppDbContext dbContext,
-		IOptionsSnapshot<AppSetting> snapshot,
 		ILogger<McToolService> logger
-	) : base(dbContext: dbContext, snapshot: snapshot) {
+	) {
 		this.logger = logger;
 	}
 
 	public async Task<ApiResponse> VerifySignIn(string username, string password) {
-		var passwordHasher = new PasswordHasher<UserModel>();
-		var user = await this.dbContext.users.FirstOrDefaultAsync(m => m.code == username || m.email == username);
-		if (user is null || user.password is null) {
-			return new ApiBadRequestResponse("No user");
-		}
-		if (passwordHasher.VerifyHashedPassword(user, user.password, password) == PasswordVerificationResult.Failed) {
-			return new ApiUnauthorizedResponse("Invalid");
-		}
+		// var passwordHasher = new PasswordHasher<UserModel>();
+		// var user = await this.dbContext.users.FirstOrDefaultAsync(m => m.code == username || m.email == username);
+		// if (user is null || user.password is null) {
+		// 	return new ApiBadRequestResponse("No user");
+		// }
+		// if (passwordHasher.VerifyHashedPassword(user, user.password, password) == PasswordVerificationResult.Failed) {
+		// 	return new ApiUnauthorizedResponse("Invalid");
+		// }
 
 		return new ApiSuccessResponse();
 	}
